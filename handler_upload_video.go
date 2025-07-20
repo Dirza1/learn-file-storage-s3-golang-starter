7be55e7cbba9fe503f193019050caa06d84c00d9
Dirza1 <jasper.olthof@xs4all.nl>
2025-07-20 22:14:26 +0200

    added stuf

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -2,6 +2,48 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+
+	maxUploadLImit := http.MaxBytesReader(w, r.Body, 1<<30)
+	r.Body = maxUploadLImit
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	videoMeta, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	if videoMeta.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "unautorised user", err)
+		return
+	}
+	r.ParseMultipartForm(1 << 30)
+
+	videoFile, videoHeader, err := r.FormFile("video")
+	defer videoFile.Close()
+
+}
